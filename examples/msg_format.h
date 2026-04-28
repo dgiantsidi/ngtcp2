0bc3b46474335993aea87a8017bdc2853f6260d7
dgiantsidi <dimitra.giantsidi@gmail.com>
2026-04-28 21:14:19 +0000

    udpates from locacl-vm

--- examples/msg_format.h
+++ examples/msg_format.h
@@ -4,10 +4,12 @@ constexpr int k_local_server_port = 7000;
 // body of message format for HTTP/3
 struct http_submit_msg {
   uint64_t request_id;
+  uint64_t latest_covered_request_id; // for optimization: the latest request id that covers all previous requests (i.e., the request with the largest request_id among the requests that cover all previous requests)
   uint64_t zil_blk_id;
   char poolname[ZFS_MAX_DATASET_NAME_LEN];
   char commitment[ZFS_MAX_DATASET_NAME_LEN];
   uint64_t ts;
+  int blk_type; //this should be either TAIL or UB;
 };
 
 // body of message format for HTTP/3
@@ -15,6 +17,8 @@ struct http_reponse_msg {
   uint64_t request_id;
   uint64_t zil_blk_id;
   uint64_t ccf_commit_seqno;
+  char commitment[ZFS_MAX_DATASET_NAME_LEN];
+  int blk_type; //this should be either TAIL or UB;
 };
 
 using http_submit_msg_t = struct http_submit_msg;
